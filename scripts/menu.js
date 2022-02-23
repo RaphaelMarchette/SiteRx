2ab4855f18f28da5f7620620b064c0d4183dc044
RaphaelMarchette <raphael.marchette23@gmail.com>
2022-02-23 00:05:25 -0300

    rx

---
+++ scripts/menu.js
@@ -0,0 +1,11 @@
+function fecharModal()
+{
+  document.getElementById('fundo').style.display = 'none';
+  document.getElementById('modal').style.display = 'none';
+}
+
+function abrirModal()
+{
+  document.getElementById('fundo').style.display = 'block';
+  document.getElementById('modal').style.display = 'block';
+}
